006161a692c97d65ea32adc0d1d0f01d96538fcf
Safarovv06 <safarovusa@gmail.com>
2023-06-14 14:53:31 +0600

    yusuf work

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React, { useState} from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css"
 import AuthDetails from "../AuthDetails/AuthDetails";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import { faFontAwesome } from "@fortawesome/free-regular-svg-icons";
 function Navbar() {
         const [authUser, setAuthUser] = useState(null);
         return (
@@ -11,12 +13,10 @@ function Navbar() {
                 <h1 className="item ava">Mega-Team's Movie</h1>
                 <div className="item"><NavLink to="/home" >Home</NavLink></div>
                 <div className="item"><NavLink to="movies" >Films</NavLink></div>
-                
                 <div className="item">
-                        
                         <NavLink to="/SignIn">Sign in</NavLink>
                 </div>
-               
+                
                 <AuthDetails/>
         
         </nav>
